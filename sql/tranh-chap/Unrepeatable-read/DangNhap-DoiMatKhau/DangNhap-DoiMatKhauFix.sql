cdce3fd0760a47099b0cd6fa060b6c960f65b90e
21120321 <gamefun206@gmail.com>
2024-01-15 21:01:50 +0700

    doi cach lam login

--- sql/tranh-chap/Unrepeatable-read/DangNhap-DoiMatKhau/DangNhap-DoiMatKhauFix.sql
+++ sql/tranh-chap/Unrepeatable-read/DangNhap-DoiMatKhau/DangNhap-DoiMatKhauFix.sql
@@ -4,11 +4,21 @@ create proc  proc_DangNhap @username char(255) , @password char(255)
 As
 Begin
 	-- Kiem tra ton tai tai khong trong he thong hay khong 
+	Begin tran
+	IF (not exists (Select * from Account  with (UPDLOCK)
+	Where username = @username and password = @password and accountType != 'Lock'))
+	Begin
+		Print 'Tai khoan khong dung'
+		Rollback tran
+		Return 1
+	End
 	Waitfor Delay '00:00:3';
-	
 	Select * from Account where username = @username and password = @password
 	and accountType != 'Lock'
+	Commit tran
 End 
+
+Go
 
 
 drop proc if exists proc_DoiMatKhau
